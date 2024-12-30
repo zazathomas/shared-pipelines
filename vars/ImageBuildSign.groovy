1a7a7c83dbbe12e7506ff3a00a488e84e3be4703
Zaza <thomas.kyle6@gmail.com>
2024-12-30 18:32:42 +0000

    Update ImageBuildSign.groovy

--- vars/ImageBuildSign.groovy
+++ vars/ImageBuildSign.groovy
@@ -1,4 +1,12 @@
 def call(Map config = [:]) {
+    properties([
+    parameters([
+        string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/zazathomas/Jenkins-for-DevSecOps.git', description: 'Repository containing the Dockerfile'),
+        string(name: 'KANIKO_CONTEXT', defaultValue: 'Docker-Deployment', description: 'Context for Docker build'),
+        string(name: 'PATH_TO_DOCKERFILE', defaultValue: 'Docker-Deployment/Dockerfile', description: 'Path to Dockerfile'),
+        string(name: 'IMAGE_NAME', defaultValue: '', description: 'Image name to build'),
+        string(name: 'IMAGE_TAG', defaultValue: '', description: 'Specify the image tag')
+    ])
     // Define variables
     def registry = 'ghcr.io'
     def image_repository = 'zazathomas'
